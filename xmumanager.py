a20d12d5cb9a906b408dd5bd7c06ebc89218c348
ChenXiaoHan1997 <31896176+ChenXiaoHan1997@users.noreply.github.com>
2017-09-20 17:03:07 +0800

    Update xmumanager.py

--- xmumanager.py
+++ xmumanager.py
@@ -7,7 +7,7 @@ if __name__=='__main__':
 
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
 
-    #XMUworker.read_all_rooms()
-    #XMUworker.crawl_all_courses()
+    XMUworker.read_all_rooms()
+    XMUworker.crawl_all_courses()
     XMUworker.query_empty_rooms(5, 3, 1)
-    #XMUworker.write_empty_rooms()
+    XMUworker.write_empty_rooms()
